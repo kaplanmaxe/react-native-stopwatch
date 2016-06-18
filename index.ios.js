c94d18ed5bbe073af51d10ecb0f05b3d2ef9f7e0
Max Kaplan <kaplanmaxe@users-MacBook-Pro.local>
2016-06-18 17:22:04 -0400

    leaving off at video 25

---
+++ index.ios.js
@@ -0,0 +1,75 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+var Stopwatch = React.createClass({
+  render: function() {
+    return <View style={styles.container}>
+      <View style={[styles.header, this.border('yellow')]}>
+
+        <View style={this.border('red')}>
+          <Text>
+            00:00:00
+          </Text>
+        </View>
+
+        <View style={this.border('green')}>
+          {this.startStopButton()}
+          {this.lapButton()}
+        </View>
+      </View>
+
+      <View style={[styles.footer, this.border('blue')]}>
+        <Text>
+          I am a list of laps
+        </Text>
+      </View>
+
+    </View>
+  },
+  startStopButton: function() {
+    return <View>
+      <Text>
+        Start
+      </Text>
+    </View>
+  },
+  lapButton: function() {
+    return <View>
+      <Text>
+        Lap
+      </Text>
+    </View>
+  },
+  border: function(color) {
+    return {
+      borderColor: color,
+      borderWidth: 4
+    }
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+  },
+  header: {
+    flex: 1
+  },
+  footer: {
+    flex: 1
+  }
+});
+
+AppRegistry.registerComponent('stopwatch', () => Stopwatch);
